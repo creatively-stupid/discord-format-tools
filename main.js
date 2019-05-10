5d4fba23d3a787deb6544e261abd29cea120fc03
Michael <michael@malinov.com>
2019-05-10 10:49:40 -0700

    yes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var clipboard = require("clipboardy");
+var fs = require("fs");
 
 process.on('SIGINT', () => {
     console.log("\nExiting...");
@@ -21,10 +21,58 @@ var converters = [
                 o += s[i];
             }
         }
+        return o;
+    },
+    (s) => {
+        var arr = {
+            "a": "ᴀ",
+            "b": "ʙ",
+            "c": "ᴄ",
+            "d": "ᴅ",
+            "e": "ᴇ",
+            "f": "ғ",
+            "g": "ɢ",
+            "h": "ʜ",
+            "i": "ɪ",
+            "j": "ᴊ",
+            "k": "ᴋ",
+            "l": "ʟ",
+            "m": "ᴍ",
+            "n": "ɴ",
+            "o": "ᴏ",
+            "p": "ᴘ",
+            "q": "ǫ",
+            "r": "ʀ",
+            "s": "s",
+            "t": "ᴛ",
+            "u": "ᴜ",
+            "v": "ᴠ",
+            "w": "ᴡ",
+            "x": "x",
+            "y": "ʏ",
+            "z": "ᴢ"
+        };
+        var o = "";
+        for (var i = 0; i < s.length; i++) {
+            o += "||**~~__*`"+s[i]+"`*__~~**||";
+        }
+        return o;
+    },
+    (s) => {
+    };
+    var o = "";
+    for (var i = 0; i < s.length; i++) {
+        if (arr[s[i]]) {
+            o += arr[s[i]];
+        } else {
+            o += s[i];
+        }
+    }
+    return o;
     }
 ];
 var askstage = -1;
-var tree = [0, "", "", ""];
+var tree = 0;
 var handleData = (d) => {
     var str = d.toString().trim();
     ( // thanks reddit I now use this instead of switch case
@@ -36,7 +84,7 @@ var handleData = (d) => {
                     console.log("");
                     process.stdout.write("> ");
                     askstage = 1;
-                    tree[0] = 1;
+                    tree = 0;
                 }:
                 (str === "2")?()=>{
                     console.log("chose small caps (unicode style)");
@@ -44,7 +92,7 @@ var handleData = (d) => {
                     console.log("");
                     process.stdout.write("> ");
                     askstage = 1;
-                    tree[0] = 2;
+                    tree = 1;
                 }:
                 (str === "3")?()=>{
                     console.log("chose crazy text");
@@ -52,7 +100,7 @@ var handleData = (d) => {
                     console.log("");
                     process.stdout.write("> ");
                     askstage = 1;
-                    tree[0] = 3;
+                    tree = 2;
                 }:
                 ()=>{
                 console.log("invalid option! Valid options are:");
@@ -65,23 +113,18 @@ var handleData = (d) => {
             )();
         }:
         (askstage === 1)?()=>{
-            (
-                (tree[0] === 1)?()=>{
-                    console.log("converting...");
-                    tree[1] = str;
-                    process.stdout.write("> ");
-                }:
-                (tree[0] === 2)?()=>{
-                    console.log("converting...");
-                    tree[1] = str;
-                    process.stdout.write("> ");
-                }:
-                (tree[0] === 3)?()=>{
-                    console.log("converting...");
-                    tree[1] = str;
-                    process.stdout.write("> ");
-                }:
-            error)();
+            console.log("converting...");
+            var o = converters[tree](str);
+            console.log("done!");
+            console.log("output is " + o.length + " characters long!");
+            console.log("storing output in " + process.cwd() + "out.txt");
+            fs.writeFileSync('out.txt', o);
+            console.log("saved!");
+            console.log("raw text:\n");
+            console.log(o);
+            console.log("");
+            console.log("thank you for using Dɪsᴄᴏʀᴅ-Fᴏʀᴍᴀᴛ-Tᴏᴏʟs");
+            process.exit(0);
         }:
     error)();
 };
