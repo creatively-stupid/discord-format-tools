ca951e759117e30f929cbe00338558a4502f9aa7
Michael <michael@malinov.com>
2019-05-10 15:03:35 -0700

    ok i made this

--- main.js
+++ main.js
@@ -1,142 +1,58 @@
-var fs = require("fs");
+var fs = require("fs"),
+    converters = require("./converters"),
+    cli = require("./cli"),
+    chalk = require("chalk");
 
-process.on('SIGINT', () => {
-    console.log("\nExiting...");
-    process.exit(0);
-});
-function error() {
-    console.log("\nERROR INCOUNTERED! \nExiting...");
-    process.exit(0);
-}
-String.prototype.isLowerCase = () => {
-    return /^[a-z]*$/.test(this.valueOf());
-};
-var converters = [
-    (s) => {
-        var o = "";
-        for (var i = 0; i < s.length; i++) {
-            if (s[i].isLowerCase()) {
-                o += "\\:regional_indicator_" + s[i] + ":";
-            } else {
-                o += s[i];
-            }
-        }
-        return o;
-    },
-    (s) => {
-        var arr = {
-            "a": "ᴀ",
-            "b": "ʙ",
-            "c": "ᴄ",
-            "d": "ᴅ",
-            "e": "ᴇ",
-            "f": "ғ",
-            "g": "ɢ",
-            "h": "ʜ",
-            "i": "ɪ",
-            "j": "ᴊ",
-            "k": "ᴋ",
-            "l": "ʟ",
-            "m": "ᴍ",
-            "n": "ɴ",
-            "o": "ᴏ",
-            "p": "ᴘ",
-            "q": "ǫ",
-            "r": "ʀ",
-            "s": "s",
-            "t": "ᴛ",
-            "u": "ᴜ",
-            "v": "ᴠ",
-            "w": "ᴡ",
-            "x": "x",
-            "y": "ʏ",
-            "z": "ᴢ"
-        };
-        var o = "";
-        for (var i = 0; i < s.length; i++) {
-            o += "||**~~__*`"+s[i]+"`*__~~**||";
-        }
-        return o;
-    },
-    (s) => {
-    };
-    var o = "";
-    for (var i = 0; i < s.length; i++) {
-        if (arr[s[i]]) {
-            o += arr[s[i]];
-        } else {
-            o += s[i];
-        }
-    }
-    return o;
-    }
-];
-var askstage = -1;
-var tree = 0;
-var handleData = (d) => {
-    var str = d.toString().trim();
+var askstage = 0;
+var choice = 0;
+cli((str) => {
     ( // thanks reddit I now use this instead of switch case
         (askstage === 0)?()=>{
             (
-                (str === "1")?()=>{
-                    console.log("chose small caps (discord style)");
-                    console.log("enter message to be converted:");
-                    console.log("");
-                    process.stdout.write("> ");
+                (converters[parseInt(str)-1])?()=>{
+                    choice = parseInt(str)-1;
+                    cli.write(chalk.rgb(127, 127, 127)("chose "))
+                        .write(converters[choice].n)
+                        .write(chalk.rgb(127, 127, 127)("\nenter message to be converted:\n\n"))
+                        .write(chalk.rgb(255, 0, 0)("> "));
                     askstage = 1;
-                    tree = 0;
-                }:
-                (str === "2")?()=>{
-                    console.log("chose small caps (unicode style)");
-                    console.log("enter message to be converted:");
-                    console.log("");
-                    process.stdout.write("> ");
-                    askstage = 1;
-                    tree = 1;
-                }:
-                (str === "3")?()=>{
-                    console.log("chose crazy text");
-                    console.log("enter message to be converted:");
-                    console.log("");
-                    process.stdout.write("> ");
-                    askstage = 1;
-                    tree = 2;
                 }:
                 ()=>{
-                console.log("invalid option! Valid options are:");
-                console.log(" 1. small caps (discord style)");
-                console.log(" 2. small caps (unicode style)");
-                console.log(" 3. crazy text");
-                console.log("");
-                process.stdout.write("> ");
+                cli.write("invalid option! Valid options are:\n");
+                printchoices();
                 }
             )();
         }:
         (askstage === 1)?()=>{
-            console.log("converting...");
-            var o = converters[tree](str);
-            console.log("done!");
-            console.log("output is " + o.length + " characters long!");
-            console.log("storing output in " + process.cwd() + "out.txt");
+            cli.write(chalk.rgb(127, 127, 127)("converting...\n"));
+            var o = converters[choice].f(str);
+            cli.write(chalk.rgb(127, 127, 127)("done!\noutput is "))
+                .write(o.length)
+                .write(chalk.rgb(127, 127, 127)(" characters long!\nstoring output in "))
+                .write(process.cwd())
+                .write("/out.txt")
+                .write(chalk.rgb(127, 127, 127)("...\n"));
             fs.writeFileSync('out.txt', o);
-            console.log("saved!");
-            console.log("raw text:\n");
-            console.log(o);
-            console.log("");
-            console.log("thank you for using Dɪsᴄᴏʀᴅ-Fᴏʀᴍᴀᴛ-Tᴏᴏʟs");
-            process.exit(0);
+            cli.write(chalk.rgb(127, 127, 127)("saved!\nraw text:\n\n"))
+                .write(o)
+                .write(chalk.rgb(127, 127, 0)("\n\nthank you for using "))
+                .write(chalk.rgb(0, 0, 255)("Dɪsᴄᴏʀᴅ-Fᴏʀᴍᴀᴛ-Tᴏᴏʟs"))
+                .write("\n")
+                .exit();
         }:
-    error)();
-};
+    cli.error)();
+}).write(chalk.rgb(127, 127, 0)("Welcome to "))
+    .write(chalk.rgb(0, 0, 255)("Dɪsᴄᴏʀᴅ-Fᴏʀᴍᴀᴛ-Tᴏᴏʟs"))
+    .write(chalk.rgb(127, 127, 127)("\n\nselect your tool:\n"));
+printchoices();
 
-console.log("Welcome to Dɪsᴄᴏʀᴅ-Fᴏʀᴍᴀᴛ-Tᴏᴏʟs");
-console.log("");
-console.log("select your tool:");
-console.log(" 1. small caps (discord style)");
-console.log(" 2. small caps (unicode style)");
-console.log(" 3. crazy text");
-console.log("");
-process.stdout.write("> ");
-askstage = 0;
-
-process.stdin.addListener("data", handleData);
+function printchoices() {
+    for (var i = 0; i < converters.length; i++) {
+        cli.write(chalk.rgb(0, 255, 255)((i+1).toString()))
+            .write(chalk.rgb(0, 255, 255)(") "))
+            .write(converters[i].n)
+            .write("\n");
+    }
+    cli.write("\n")
+        .write(chalk.rgb(255, 0, 0)("> "));
+}
